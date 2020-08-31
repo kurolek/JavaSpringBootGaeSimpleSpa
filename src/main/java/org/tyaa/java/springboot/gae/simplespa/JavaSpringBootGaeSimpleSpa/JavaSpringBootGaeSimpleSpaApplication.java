93dbcbdaa58122104216638309a3291c6c4bb035
YuriiTrofimenko <tyaa@ukr.net>
2020-08-31 15:04:40 +0300

    init

---
+++ src/main/java/org/tyaa/java/springboot/gae/simplespa/JavaSpringBootGaeSimpleSpa/JavaSpringBootGaeSimpleSpaApplication.java
@@ -0,0 +1,13 @@
+package org.tyaa.java.springboot.gae.simplespa.JavaSpringBootGaeSimpleSpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootGaeSimpleSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootGaeSimpleSpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tyaa/java/springboot/gae/simplespa/JavaSpringBootGaeSimpleSpa/JavaSpringBootGaeSimpleSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tyaa.java.springboot.gae.simplespa.JavaSpringBootGaeSimpleSpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootGaeSimpleSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
